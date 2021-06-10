c3aaacd2c615db25dd3445cf8f4a9556c061f2ec
jae-yeal-Seo <74850268+jae-yeal-Seo@users.noreply.github.com>
2021-06-11 02:10:22 +0900

    Add files via upload
    app.get부터는 다음에 해석!

---
+++ auth.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/join', isNotLoggedIn, async (req, res, next) => {
+  const { email, nick, password } = req.body;
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      return res.redirect('/join?error=exist');
+    }
+    const hash = await bcrypt.hash(password, 12);
+    await User.create({
+      email,
+      nick,
+      password: hash,
+    });
+    return res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+// /join에다가 email, nick, password를 구조분해 할당으로 받는다. 
+// email을 보고서 User의 모델에 존자해면 오류를 보낸다. 
+// 그리고 바로 /로 리다이렉트 해준다. 우리눈에는 /join이 안보인다. 바로 리다이렉트를 하기 때문에.
+
+router.post('/login', isNotLoggedIn, (req, res, next) => {
+  passport.authenticate('local', (authError, user, info) => {
+    if (authError) {
+      console.error(authError);
+      return next(authError);
+    }
+    if (!user) {
+      return res.redirect(`/?loginError=${info.message}`);
+    }
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return next(loginError);
+      }
+      return res.redirect('/');
+    });
+  })(req, res, next); // 미들웨어 내의 미들웨어에는 (req, res, next)를 붙인다.
+});
+//
+
+router.get('/logout', isLoggedIn, (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+});
+
+router.get('/kakao', passport.authenticate('kakao'));
+
+router.get('/kakao/callback', passport.authenticate('kakao', {
+  failureRedirect: '/',
+}), (req, res) => {
+  res.redirect('/');
+});
+
+module.exports = router;
+//이 js는 app.use('/auth', authRouter);로써 미들웨어로 쓰인다. 그래서 /auth가 전제되어있다.거기에
+//추가로 /kakao, /kakao/callback, /join, /login이 붙는 것이다.
